12b16a864ed520f6b91b126ac0cb8fa9ec3a663d
Luong Tan Phat <tanphat9324@gmail.com>
2020-09-26 20:20:29 +0700

    add firebaseConfig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BauCuaA';
+  constructor(private http: HttpClient) {
+  }
+  list = [
+    {
+      name: "Anna",
+      age: 26
+    }, {
+      name: "Leona",
+      age: 31
+    }
+  ]
+  ngOnInit() {
+    this.http.post("https://baucua-cef83.firebaseio.com/posts.json",this.list).subscribe(res=>{
+      console.log(res);
+    })
+  }
 }
