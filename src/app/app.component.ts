a23ff8fccc5fd8a7d682159541040197c985b8ef
Lương Tấn Phát <50788028+tanphat9324@users.noreply.github.com>
2020-09-27 18:59:16 +0700

    Revert "Setup project"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'BauCuaA';
-  constructor(private http: HttpClient) {
-  }
-  list = [
-    {
-      name: "Anna",
-      age: 26
-    }, {
-      name: "Leona",
-      age: 31
-    }
-  ]
-  ngOnInit() {
-    this.http.post("https://baucua-cef83.firebaseio.com/posts.json",this.list).subscribe(res=>{
-      console.log(res);
-    })
-  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCeSFdzM0fMryMPPXdo0AJ5NzPKel2QpXI",
-    authDomain: "baucua-cef83.firebaseapp.com",
-    databaseURL: "https://baucua-cef83.firebaseio.com",
-    projectId: "baucua-cef83",
-    storageBucket: "baucua-cef83.appspot.com",
-    messagingSenderId: "254040381376",
-    appId: "1:254040381376:web:78e1120b16c1a1c8c59a25",
-    measurementId: "G-8V2FW3SS90"
-  }
+  production: false
 };
 
 /*
